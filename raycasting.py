4da607d8ccc4a66a0b690574188807cf2fe552cb
Brunovncs <brunoviniciusrp@gmail.com>
2023-12-29 12:57:20 -0300

    doom-like

---
+++ raycasting.py
@@ -0,0 +1,25 @@
+import pygame as pg
+import math
+from settings import *
+
+class RayCasting:
+    def __init__(self, game):
+        self.game = game
+        
+    def ray_cast(self):
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+        
+        ray_angle = self.game.player.angle - HALF_FOV + 0.0001        
+        for ray in range(NUM_RAYS):
+            sin_a = math.sin(ray_angle)
+            cos_a = math.sin(ray_angle)
+            
+            # verticals
+            
+            
+            
+            ray_angle += DELTA_ANGLE
+    
+    def update(self):
+        self.ray_cast()
